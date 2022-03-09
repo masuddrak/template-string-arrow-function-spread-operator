cd9efe20ea4356b4878bf234112ff62ab1a58c7f
masud rana <masudranaacb@gmail.com>
2022-03-09 11:26:06 -0800

    templetString using

---
+++ app.js
@@ -0,0 +1,8 @@
+const numbers=[4,8,98,56,2,3]
+const student={
+    name:'Salam Khan',
+    Age:55,
+    study:'Mathematics'
+}
+const templetStriing=`My Name Is ${student.name},My study On ${student.study},My Age ${student.Age},My Roll ${numbers[2]}`
+console.log(templetStriing)
